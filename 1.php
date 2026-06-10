636839461a42fe0f940868654ffa9326d87c471f
zenithmkw <zenith.makwana126980@marwadiuniversity.ac.in>
2026-06-10 13:03:07 +0530

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,13 @@
+<?php
+const unm = "MU";
+const sem = 3;
+$result = "pass";
+$name = "zenith";
+$cgpa = 7.39;
+
+echo "university name".unm."</br>"; 
+echo "name of student".$name."</br>";
+echo "semester : ".sem."</br>";
+echo "result of sem 3".$result."</br>";
+echo "cgpa".$cgpa."</br>";
+?>
