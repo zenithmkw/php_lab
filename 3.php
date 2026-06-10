3aa389a85716ae84fd1ee6b86cfc46601b9c6ead
zenithmkw <zenith.makwana126980@marwadiuniversity.ac.in>
2026-06-10 13:05:00 +0530

    Add files via upload

---
+++ 3.php
@@ -0,0 +1,12 @@
+<?php
+$n1 = 100;
+$n2 = 10;
+
+echo "addition is". $n1+$n2 ."<br>";
+
+echo "subsraction is". $n1-$n2."<br>";
+
+echo "multiplication is". $n1*$n2."<br>";
+
+echo "division is". $n1/$n2."<br>";
+?>
