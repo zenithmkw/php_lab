7ded91a74630a58101058d7c5b7ce93921f794ad
zenithmkw <zenith.makwana126980@marwadiuniversity.ac.in>
2026-06-12 12:41:53 +0530

    Add files via upload

---
+++ prog6.php
@@ -0,0 +1,18 @@
+<?php
+echo "15 to 20 using do while"."<br/>";
+$n=15;
+do{
+echo "number".$n."<br/>";
+$n++;
+}while($n<=20);
+
+echo "15to20 using while"."<br/>";
+$j=15;
+while($j<=20){
+    echo "number".$j."<br/>";
+    $j++;
+}
+
+
+
+?>
