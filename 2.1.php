837c78495ed1f045bbf2eed3725435f74ccb9d6a
zenithmkw <zenith.makwana126980@marwadiuniversity.ac.in>
2026-07-01 10:34:24 +0530

    Add files via upload

---
+++ 2.1.php
@@ -0,0 +1,42 @@
+<?php
+
+$days = array("monday", "tuesday", "wednesday", "thursday", "friday", "satuday", "sunday");
+print_r($days);
+
+echo "<br><br>";
+
+$month = array(
+"jan" => 31, 
+"feb" => 28,
+"march" => 31,
+"april" => 30,
+"may" => 31,
+"june" => 30,
+"july" => 31,
+"aug" => 30,
+"sep" => 31,
+"oct" => 31,
+"nov" => 30,
+"dec" => 31,
+);
+print_r($month);
+
+echo "<br><br>";
+
+
+$laptop = array(
+"hp" => array(
+    "model" => "victus",
+    "price" => "50000"
+),
+
+"dell" => array(
+    "model" => "I34B6",
+    "price" => "70000"
+)
+);
+
+print_r($laptop);
+
+
+?>
