a3e149dc5ebc30a891fe87fb77ab4a52eb33cad6
zenithmkw <zenith.makwana126980@marwadiuniversity.ac.in>
2026-07-10 10:14:01 +0530

    Add files via upload

---
+++ 2.6.php
@@ -0,0 +1,7 @@
+<?php
+echo "DAYOFWEEK(): " . date("w") + 1 . "<br>";   
+echo "WEEKDAY(): " . date("N") - 1 . "<br>";     
+echo "DAYOFMONTH(): " . date("d") . "<br>";      
+echo "DAYOFYEAR(): " . date("z") + 1 . "<br>";   
+echo "DAYNAME(): " . date("l") . "<br>";         
+?>
