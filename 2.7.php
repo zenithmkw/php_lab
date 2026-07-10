4c1f0ff87b9c944193a279c111f6b15d75e180dc
zenithmkw <zenith.makwana126980@marwadiuniversity.ac.in>
2026-07-10 10:14:18 +0530

    Add files via upload

---
+++ 2.7.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "HOUR: ";
+echo date("H");
+
+echo "<br>";    
+
+echo "MINUTE: ";
+echo date("i");
+
+echo "<br>";    
+echo "SECONDS: ";
+echo date("s");
+
+echo "<br>";    
+echo "Date Format: ";
+echo date("d/m/y");
+
+
+?>
