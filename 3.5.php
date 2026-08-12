9332a84cdfc4f11323de2f7519b73dbada298092
zenithmkw <zenith.makwana126980@marwadiuniversity.ac.in>
2026-08-12 10:23:34 +0530

    Add files via upload

---
+++ 3.5.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form method="post">
+    
+    username:<input type="text" name="username">
+    password:<input type="password" name="password">
+    <input type="submit">
+
+</form>
+
+
+</body>
+</html>
+
+<?php
+   session_start();
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $username = $_POST['username'];
+        $pass = $_POST['password'];
+        
+        $_SESSION['name']= $username;
+        header('location: homepage.php');
+      
+      
+       
+    }
+
+
+
+?>
